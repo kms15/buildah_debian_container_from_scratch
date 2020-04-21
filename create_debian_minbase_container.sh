8aa6b94c9b21094f40c62ab7e24ea41b8b14f884
Kendrick Shaw <kms15@case.edu>
2020-04-21 18:35:18 +0000

    add comments and clean up container

--- create_debian_minbase_container.sh
+++ create_debian_minbase_container.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# create a new buildah container from scratch
 container=$(buildah from scratch)
 
 # Unshare is required for buildah mount to work without superuser privileges.
-buildah unshare bash -c "mount_point=\$(buildah mount $container) &&\
+buildah unshare bash -c "\
+`# mount the container's filesystem`\
+mount_point=\$(buildah mount $container) &&\
+`# install a debian minbase installation in the container's filesystem`\
 fakeroot debootstrap --variant=minbase --arch=amd64 buster \$mount_point http://deb.debian.org/debian ;\
+`# unmount the container's filesystem to clean up`\
 buildah unmount $container\
 "
 
+# create an image from this container
 buildah commit $container $USER/debian_buster_minbase
+
+# delete the container we used to build the image
+buildah rm $container
